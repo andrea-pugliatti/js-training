ce47ac5da8464726cec12bc59f7be333cb9f2719
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-12-23 20:58:14 +0100

    Cleanup project

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 export default function Homepage() {
 	return (
-		<>
+		<div className="container">
 			<h1>JS Training</h1>
 			<Link to={"/calculator"}>
 				<div>Calcolatrice</div>
 			</Link>
-		</>
+		</div>
 	);
 }
