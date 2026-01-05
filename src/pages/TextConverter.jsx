5c4fd6bec9537086f28da6ff3b97492fefe55299
andrea-pugliatti <andrea.pugliatti@outlook.it>
2026-01-05 10:11:42 +0100

    Add logic for text transformation

--- src/pages/TextConverter.jsx
+++ src/pages/TextConverter.jsx
@@ -3,13 +3,27 @@ import { useState } from "react";
 export default function TextConverter() {
 	const [inputString, setInputString] = useState("");
 
+	const upperCase = (string) => string.toUpperCase();
+
+	const lowerCase = (string) => string.toLowerCase();
+
+	const capitalize = (string) =>
+		string
+			.split(" ")
+			.map(
+				(word) => word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase(),
+			)
+			.join(" ");
+
+	const reversed = (string) => string.split("").toReversed();
+
 	return (
 		<div className="container">
 			<h1>Convertitore di Testo</h1>
 
 			<input
-				id="first-number-input"
-				name="first-number-input"
+				id="string-input"
+				name="string-input"
 				type="text"
 				value={inputString}
 				onChange={(e) => setInputString(e.target.value)}
@@ -17,22 +31,22 @@ export default function TextConverter() {
 
 			<div className="upper-case">
 				<h3>Maiuscolo</h3>
-				<span>{inputString}</span>
+				<span>{upperCase(inputString)}</span>
 			</div>
 
 			<div className="lower-case">
 				<h3>Minuscolo</h3>
-				<span>{inputString}</span>
+				<span>{lowerCase(inputString)}</span>
 			</div>
 
 			<div className="capitalize">
 				<h3>Iniziale maiuscola</h3>
-				<span>{inputString}</span>
+				<span>{capitalize(inputString)}</span>
 			</div>
 
 			<div className="inverted-string">
 				<h3>Frase invertita</h3>
-				<span>{inputString}</span>
+				<span>{reversed(inputString)}</span>
 			</div>
 		</div>
 	);
