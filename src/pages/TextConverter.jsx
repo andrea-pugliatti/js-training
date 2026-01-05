7a21aa8ef44b6f7dbceac3439f33411ba6457faf
andrea-pugliatti <andrea.pugliatti@outlook.it>
2026-01-05 10:30:50 +0100

    Add styles

--- src/pages/TextConverter.jsx
+++ src/pages/TextConverter.jsx
@@ -19,34 +19,36 @@ export default function TextConverter() {
 
 	return (
 		<div className="container">
-			<h1>Convertitore di Testo</h1>
+			<div className="text-converter">
+				<h1>Convertitore di Testo</h1>
 
-			<input
-				id="string-input"
-				name="string-input"
-				type="text"
-				value={inputString}
-				onChange={(e) => setInputString(e.target.value)}
-			/>
+				<input
+					id="string-input"
+					name="string-input"
+					type="text"
+					value={inputString}
+					onChange={(e) => setInputString(e.target.value)}
+				/>
 
-			<div className="upper-case">
-				<h3>Maiuscolo</h3>
-				<span>{upperCase(inputString)}</span>
-			</div>
+				<div className="upper-case">
+					<span>Maiuscolo: </span>
+					<span>{upperCase(inputString)}</span>
+				</div>
 
-			<div className="lower-case">
-				<h3>Minuscolo</h3>
-				<span>{lowerCase(inputString)}</span>
-			</div>
+				<div className="lower-case">
+					<span>Minuscolo: </span>
+					<span>{lowerCase(inputString)}</span>
+				</div>
 
-			<div className="capitalize">
-				<h3>Iniziale maiuscola</h3>
-				<span>{capitalize(inputString)}</span>
-			</div>
+				<div className="capitalize">
+					<span>Iniziale maiuscola: </span>
+					<span>{capitalize(inputString)}</span>
+				</div>
 
-			<div className="inverted-string">
-				<h3>Frase invertita</h3>
-				<span>{reversed(inputString)}</span>
+				<div className="inverted-string">
+					<span>Frase invertita: </span>
+					<span>{reversed(inputString)}</span>
+				</div>
 			</div>
 		</div>
 	);
