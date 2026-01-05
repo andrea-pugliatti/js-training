b4ab8abc999d505dc55bb666f2837d7ff73a08c2
andrea-pugliatti <andrea.pugliatti@outlook.it>
2026-01-05 11:11:24 +0100

    Add oddEvenFromArray function and test

--- src/pages/OddEven.jsx
+++ src/pages/OddEven.jsx
@@ -1,5 +1,24 @@
+import { useEffect } from "react";
+
 export default function OddEven() {
-	function oddEvenFromArray() {}
+	function isEven(number) {
+		return number % 2 === 0;
+	}
+
+	function oddEvenFromArray(array) {
+		const result = { even: [], odd: [] };
+		array.forEach((current) => {
+			isEven(current) ? result.even.push(current) : result.odd.push(current);
+		});
+		return result;
+	}
+
+	useEffect(() => {
+		const arr = [
+			1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+		];
+		console.log(oddEvenFromArray(arr));
+	}, []);
 
 	return (
 		<div className="container">
